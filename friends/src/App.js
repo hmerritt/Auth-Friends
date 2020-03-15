e263593feb5f3854b19cbb621722fdd8c03cad4c
Harry Merritt <harry@merritt.es>
2020-03-15 23:57:09 +0000

    Setup react router

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+
+import Login from "./components/Login/Login";
+import FriendsList from "./components/FriendsList";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
-            sup
+
+
+            {/* Routes */}
+            <Switch>
+                <PrivateRoute component={FriendsList} />
+                <Route path="/login" component={Login} />
+                <Route component={Login} />
+            </Switch>
         </div>
     );
 }
